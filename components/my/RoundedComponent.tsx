8ceca6f437959c128e3d867b3f0fa994f498df7a
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-25 09:21:54 +0100

    added. circular-progress solution

--- components/my/RoundedComponent.tsx
+++ components/my/RoundedComponent.tsx
@@ -1,6 +1,6 @@
 import styles from "@/styles/common";
 import { Text, View } from "react-native";
-
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 type RoundedComponentProps = {
   inputValue: number;
   maxValue: number;
@@ -17,57 +17,24 @@ export default function RoundedComponent({
 }: RoundedComponentProps) {
   const percent: number = (inputValue / maxValue) * 100;
 
-  const rightRotation: number = percent <= 50 ? (percent / 50) * 180 - 36 : 180;
-  const leftRotation: number =
-    percent > 50 ? ((percent - 50) / 50) * 180 - 36 : 0;
-
-  const rightColor: string = percent > 0 ? color : "transparent";
-  const leftColor: string = percent > 50 ? color : "transparent";
-
-  let isFull: boolean = false;
-  if (percent === 10) isFull = true;
-
   return (
     <View style={styles.containerRoundedComponent}>
-      <View style={styles.circleContainer}>
-        <View style={styles.containerOuterCircle}></View>
-        <View style={styles.containerInnerCircleRight}>
-          <View
-            style={[
-              styles.innerSemiCircle,
-              {
-                borderRightColor: "transparent",
-                borderLeftColor: rightColor,
-                borderTopColor: rightColor,
-                borderBottomColor: percent < 25 ? "transparent" : rightColor,
-                transform: [{ rotate: `${rightRotation}deg` }],
-              },
-            ]}
-          ></View>
-        </View>
-        <View style={styles.containerInnerCircleLeft}>
-          <View
-            style={[
-              styles.innerSemiCircle,
-              {
-                borderLeftColor: "transparent",
-                borderRightColor: leftColor,
-                borderTopColor:
-                  percent > 50 && percent < 100 ? "transparent" : leftColor,
-                borderBottomColor: leftColor,
-                transform: [{ rotate: `${leftRotation}deg` }],
-              },
-            ]}
-          ></View>
-        </View>
-        <View style={styles.containerContent}>
-          <Text style={styles.contentInner}>
+      <AnimatedCircularProgress
+        size={200}
+        width={8}
+        fill={percent}
+        tintColor={color}
+      >
+        {() => (
+          <Text style={{ fontSize: 32, textAlign: "center" }}>
             {percent}
             {valueUnit}
           </Text>
-        </View>
-      </View>
-      <Text style={styles.contentOuter}>{title}</Text>
+        )}
+      </AnimatedCircularProgress>
+      <Text style={{ fontSize: 24, textAlign: "center", marginTop: 8 }}>
+        {title}
+      </Text>
     </View>
   );
 }
