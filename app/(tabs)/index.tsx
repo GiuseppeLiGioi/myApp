727a4f9b725875501b294b1daebbd8386c48573c
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-25 18:23:31 +0100

    fixed. dynamic sizes for web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,19 @@ import RoundedComponent from "@/components/my/RoundedComponent";
 import { View } from "react-native";
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <RoundedComponent
         inputValue={100}
         maxValue={100}
         title={"Rating"}
         color={"#1EA2F5"}
-        checked={true}
+        checked={false}
         valueUnit={"%"}
       />
     </View>
--- components/my/RoundedComponent.tsx
+++ components/my/RoundedComponent.tsx
@@ -23,15 +23,16 @@ export default function RoundedComponent({
   const percent: number = (inputValue / maxValue) * 100;
 
   const fadeAnim = useRef(new Animated.Value(0)).current;
-  const fontSizeValue = containerSize * 0.25;
-  const fontSizeSymbol = containerSize * 0.12;
-  const fontSizeText = containerSize * 0.18;
-  const borderSize = containerSize * 0.03;
-  const topPosition = -(containerSize * 0.02);
-  const innerBoxSize = containerSize * 0.6;
-  const widthSvg = containerSize * 0.8;
-  const strok = containerSize * 0.01;
-  const animatedWidth = containerSize * 0.8;
+  const maxContainerSize: number = 350;
+  const fontSizeValue: number = containerSize * 0.25;
+  const fontSizeSymbol: number = containerSize * 0.14;
+  const fontSizeText: number = containerSize * 0.18;
+  const borderSize: number = containerSize * 0.03;
+  const topPosition: number = -(containerSize * 0.02);
+  const innerBoxSize: number = containerSize * 0.6;
+  const widthSvg: number = containerSize * 0.8;
+  const strok: number = containerSize * 0.01;
+  const animatedWidth: number = containerSize * 0.8;
 
   useEffect(() => {
     if (percent === 100) {
@@ -48,80 +49,85 @@ export default function RoundedComponent({
   return (
     <View
       style={styles.containerRoundedComponent}
-      onLayout={(e) => setContainerSize(e.nativeEvent.layout.width)}
+      onLayout={(e) => {
+        const { width, height } = e.nativeEvent.layout;
+        setContainerSize(Math.min(width, height, maxContainerSize));
+      }}
     >
-      <AnimatedCircularProgress
-        size={containerSize}
-        width={borderSize}
-        fill={percent}
-        tintColor={color}
-        lineCap="round"
-        backgroundColor="#e0f2faff"
-      >
-        {() => (
-          <View
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            {percent === 100 && checked === true ? (
-              <Animated.View
-                style={{
-                  width: animatedWidth,
-                  height: animatedWidth,
-                  borderRadius: animatedWidth / 2,
-                  backgroundColor: "#54B435",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  opacity: fadeAnim,
-                }}
-              >
-                <Svg width={widthSvg} height={widthSvg} viewBox="0 0 40 40">
-                  <Path
-                    d="M12.5 20l5 5 9-9"
-                    stroke="#fff"
-                    strokeWidth={strok}
-                    fill="none"
-                    strokeLinecap="round"
-                  />
-                </Svg>
-              </Animated.View>
-            ) : (
-              <View
-                style={[
-                  styles.containerTextValue,
-                  { width: innerBoxSize, aspectRatio: 1 },
-                ]}
-              >
-                <Text
+      {containerSize > 0 && (
+        <AnimatedCircularProgress
+          size={containerSize}
+          width={borderSize}
+          fill={percent}
+          tintColor={color}
+          lineCap="round"
+          backgroundColor="#e0f2faff"
+        >
+          {() => (
+            <View
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {percent === 100 && checked === true ? (
+                <Animated.View
+                  style={{
+                    width: animatedWidth,
+                    height: animatedWidth,
+                    borderRadius: animatedWidth / 2,
+                    backgroundColor: "#54B435",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    opacity: fadeAnim,
+                  }}
+                >
+                  <Svg width={widthSvg} height={widthSvg} viewBox="0 0 40 40">
+                    <Path
+                      d="M12.5 20l5 5 9-9"
+                      stroke="#fff"
+                      strokeWidth={strok}
+                      fill="none"
+                      strokeLinecap="round"
+                    />
+                  </Svg>
+                </Animated.View>
+              ) : (
+                <View
                   style={[
-                    styles.textValue,
-                    { color: color, fontSize: fontSizeValue },
+                    styles.containerTextValue,
+                    { width: innerBoxSize, aspectRatio: 1 },
                   ]}
                 >
-                  {percent}
-                </Text>
-                <View style={styles.containerUnit}>
                   <Text
                     style={[
-                      styles.textUnit,
-                      { fontSize: fontSizeSymbol, top: topPosition },
+                      styles.textValue,
+                      { color: color, fontSize: fontSizeValue },
                     ]}
                   >
-                    {valueUnit}
+                    {percent}
                   </Text>
+                  <View style={styles.containerUnit}>
+                    <Text
+                      style={[
+                        styles.textUnit,
+                        { fontSize: fontSizeSymbol, top: topPosition },
+                      ]}
+                    >
+                      {valueUnit}
+                    </Text>
+                  </View>
                 </View>
-              </View>
-            )}
-          </View>
-        )}
-      </AnimatedCircularProgress>
+              )}
+            </View>
+          )}
+        </AnimatedCircularProgress>
+      )}
 
       <Text style={[styles.textBottom, { fontSize: fontSizeText }]}>
         {title}
